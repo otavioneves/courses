f64693a8693fd1b7bc13c8c769a0389e881c921b
Otavio Neves <otavio.neves@outlook.com>
2021-08-16 09:02:35 -0300

    Adicionando exercício de Calculo de IMC

---
+++ src/CalculoImc.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CalculoImc {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Digite o seu peso em kg:");
+		Double peso = scanner.nextDouble();
+
+		System.out.print("Digite a sua altura em metros: ");
+		Double altura = scanner.nextDouble();
+		
+		Double imc = peso / (altura * altura);
+		
+		System.out.println("IMC: " + imc);
+		
+		scanner.close();
+	}
+
+}
