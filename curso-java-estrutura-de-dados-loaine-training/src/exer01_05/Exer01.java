83ccae77fe8d3224e648521f70c630a624c9eba0
Otavio Neves <otavio.neves@outlook.com>
2021-10-09 20:51:20 -0300

    Exercises of course Java Data Structures and Algorithms

---
+++ curso-java-estrutura-de-dados-loaine-training/src/exer01_05/Exer01.java
@@ -0,0 +1,30 @@
+package exer01_05;
+
+public class Exer01 {
+
+	public static void main(String[] args) {
+
+		Lista<String> lista = new Lista<String>();
+		
+		lista.adiciona("A");
+		lista.adiciona("B");
+		lista.adiciona("C");
+		lista.adiciona("A");
+		lista.adiciona("B");
+		lista.adiciona("B");
+		
+		System.out.println(lista);
+		
+		lista.clear();
+
+		System.out.println(lista);
+		
+		lista.adiciona("A");
+		lista.adiciona("B");
+		lista.adiciona("C");
+		
+		System.out.println(lista);
+
+	}
+
+}
